6a82f204480cb7f104a3044e0b2568a6cd6c99b1
Sopow <88051510+Sopow@users.noreply.github.com>
2022-05-26 20:33:34 +0200

    new promise

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    if(!req.body.name || !req.body.email || !req.body.message || !req.body) {
+    if (!req.body.name || !req.body.email || !req.body.message || !req.body) {
         return res.status(400).json({
             message: 'No data provided'
         })
     }
-    axios.post('https://discord.com/api/webhooks/970771448652234803/Z-vVxElX3LQoMsMFUPrbIkydmZUUxGG_5jV_JPfNkj31xD72KF_A8gyLdkI9atqGPPy3', {
+    await axios.post('https://discord.com/api/webhooks/970771448652234803/Z-vVxElX3LQoMsMFUPrbIkydmZUUxGG_5jV_JPfNkj31xD72KF_A8gyLdkI9atqGPPy3', {
         embeds: [{
             title: `${req.body.name} has contacted you! (${req.body.email})`,
             description: req.body.message,
@@ -16,8 +16,13 @@ export default async function handler(req, res) {
                 text: 'Contact Form' + ' | ' + req.body.date
             }
         }]
-    })
-    res.status(200).json({
-        message: "Success"
+    }).then(() => {
+        res.status(200).json({
+            message: "Success"
+        })
+    }).catch(err => {
+        res.status(500).json({
+            message: err.message
+        })
     })
 }
