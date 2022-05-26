381025b21ddd06f81161840ad0632f55c8df4ab9
Sopow <88051510+Sopow@users.noreply.github.com>
2022-05-26 19:25:25 +0200

    e

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+    if(!req.body.name || !req.body.email || !req.body.message || !req.body) {
+        return res.status(400).json({
+            message: 'No data provided'
+        })
+    }
     axios.post('https://discord.com/api/webhooks/970771448652234803/Z-vVxElX3LQoMsMFUPrbIkydmZUUxGG_5jV_JPfNkj31xD72KF_A8gyLdkI9atqGPPy3', {
         embeds: [{
             title: `${req.body.name} has contacted you! (${req.body.email})`,
