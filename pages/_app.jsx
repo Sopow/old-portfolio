29171109a7372dfd45142be43bfea5530e156838
Sopow <Sopowbs@gmail.com>
2023-02-16 00:52:41 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,28 +3,29 @@ import "react-pro-sidebar/dist/css/styles.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  const img = "https://i.imgur.com/37syxRn.png"
   return (
     <>
       <Head>
         <title>Sopow</title>
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.sopow.fr/" />
+        <meta property="og:url" content="https://www.sopow.ga/" />
         <meta property="og:title" content="Sopow | Portfolio" />
         <meta
           property="og:description"
           content="Personal website created by Sopow"
         />
-        <meta property="og:image" content="https://imgur.com/37syxRn" />
+        <meta property="og:image" content={img} />
         <meta name="google-site-verification" content="tf06sdvU_f3DljkLJ8cemWHYRSgoxYhyUuym9L00BT8" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://www.sopow.fr/" />
+        <meta name="twitter:url" content="https://www.sopow.ga/" />
         <meta name="twitter:title" content="Sopow | Portfolio" />
         <meta
           name="twitter:description"
           content="Personal website created by Sopow available on Github"
         />
-        <meta name="twitter:image" content="https://imgur.com/37syxRn" />
-        <meta name="theme-color" content="#cc3399" />
+        <meta name="twitter:image" content={img} />
+        <meta name="theme-color" content="#011627" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
